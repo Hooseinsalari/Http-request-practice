2f49fb86e55329a36faa099cfa4d2ea120fd75a5
Hooseinsalari <hossein.salari002@gmail.com>
2021-11-19 10:48:38 +0330

    axios interceptor

--- http-app/src/services/HttpService.js
+++ http-app/src/services/HttpService.js
@@ -2,11 +2,35 @@ import axios from "axios";
 
 axios.defaults.baseURL = "http://localhost:3001";
 
+axios.interceptors.request.use(
+    (response) => {
+      console.log(response);
+  
+      return response;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject();
+    }
+  );
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject();
+  }
+);
+
 const http = {
-    get: axios.get,
-    post: axios.post,
-    delete: axios.delete,
-    put: axios.put
-}
+  get: axios.get,
+  post: axios.post,
+  delete: axios.delete,
+  put: axios.put,
+};
 
 export default http;
